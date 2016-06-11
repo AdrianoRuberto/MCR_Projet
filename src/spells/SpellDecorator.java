9ec763dd73a292fd097adb167a2d24034b19b550
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-06-12 01:55:18 +0200

    Add remove decorator

--- src/spells/SpellDecorator.java
+++ src/spells/SpellDecorator.java
@@ -29,4 +29,36 @@ public abstract class SpellDecorator extends Spell {
 	public String toString() {
 		return spell + " is decorated";
 	}
+
+	/**
+	 * Removes the decorator at the given position.
+	 *
+	 * @param pos the position
+	 */
+	public Spell removeAt(int pos) {
+		if (pos < 0)
+			throw new IndexOutOfBoundsException();
+
+		if (pos == 0)
+			return spell;
+		else if (spell instanceof SpellDecorator) {
+			spell = ((SpellDecorator) spell).removeAt(pos - 1);
+			return this;
+		} else {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+
+	/**
+	 * Counts the number of decorators
+	 *
+	 * @return the number of decorators
+	 */
+	public int nbDecorators() {
+		int nb = 1;
+		for (Spell tmp = spell; tmp instanceof SpellDecorator; tmp = ((SpellDecorator) tmp).spell) {
+			++nb;
+		}
+		return nb;
+	}
 }
